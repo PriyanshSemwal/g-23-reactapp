a6d9bcc1d26d955a05928aa1a1c78a8050c35b6e
BuriBuriZaemon123 <priyansh2100.be22@chitkara.edu.in>
2025-05-02 16:52:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return <div>
+    <h1>my React app</h1>
+    <h2>my react implementation with co cv</h2>
+    <h3>with github action</h3>
+    
+  </div>
+}
+
+export default App
